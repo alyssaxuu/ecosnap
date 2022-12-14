980a44c88f621517856d35f642883ec6f40aa65e
Leonor <mleonorfurtado@gmail.com>
2022-12-14 21:27:59 +0000

    :monocle_face: Training third  model

--- ml/train.py
+++ ml/train.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd 
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image_dataset_from_directory
-from  tf.keras.applications import MobileNetV3Small
+from  tf.keras.applications import MobileNetV3
 tf.__version__
 
 data_directory = "ml/seven_plastics/"
@@ -24,6 +24,10 @@ class_names = train_dataset.class_names
 print(class_names)
 train_dataset = train_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
 
+resize_and_rescale = tf.keras.Sequential([
+  tf.keras.layers.Resizing(IMG_SIZE, IMG_SIZE),
+  tf.keras.layers.Rescaling(1./255)
+])
 
 data_augmentation = tf.keras.Sequential([
   tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
@@ -32,7 +36,7 @@ data_augmentation = tf.keras.Sequential([
   tf.keras.layers.experimental.preprocessing.RandomZoom(0.1),
 ])
 
-mobile_net = tf.keras.applications.MobileNetV3Small(
+mobile_net = tf.keras.applications.MobileNetV3(
     input_shape=IMG_SHAPE,
     alpha=1.0,
     minimalistic=False,
@@ -57,7 +61,7 @@ feature_batch_average = global_average_layer(feature_batch)
 
 
 prediction_layer = tf.keras.layers.Dense(len(class_names), 
-# activation = "softmax"
+activation = "softmax"
 )
 
 
@@ -73,7 +77,7 @@ model.compile(optimizer = tf.keras.optimizers.Adam(
                                                     # learning_rate = 0.01
                                                     ),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(
-            from_logits =True
+            # from_logits =True
               ),
               metrics=['accuracy'])
 
@@ -130,8 +134,12 @@ for layer in mobile_net.layers[:fine_tune_at]:
 
 len(model.trainable_variables)
 
-model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = 0.0001),
-                         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits =True),
+
+
+model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = 0.00001),
+                         loss=tf.keras.losses.SparseCategoricalCrossentropy(
+                          # from_logits =True
+                          ),
                          metrics=['accuracy'])
 model.summary()
 
@@ -141,36 +149,42 @@ history_fine = model.fit(train_dataset,
                          validation_data=validation_dataset
                          )
 
-
-
-acc += history_fine.history['accuracy']
-val_acc += history_fine.history['val_accuracy']
-
-loss += history_fine.history['loss']
-val_loss += history_fine.history['val_loss']
-
-
-plt.figure(figsize=(8, 8))
-plt.subplot(2, 1, 1)
-plt.plot(acc, label='Training Accuracy')
-plt.plot(val_acc, label='Validation Accuracy')
-plt.ylim([0.8, 1])
-plt.plot([100-1,100-1],
-          plt.ylim(), label='Start Fine Tuning')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(2, 1, 2)
-plt.plot(loss, label='Training Loss')
-plt.plot(val_loss, label='Validation Loss')
-plt.ylim([0, 1.0])
-plt.plot([100-1,100-1],
-         plt.ylim(), label='Start Fine Tuning')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.xlabel('epoch')
-plt.show()
-
 import os
-ecosnap_save_path = os.path.join('ml', "ecosnap/1/")
+ecosnap_save_path = os.path.join('ml', "ecosnap/4/")
 tf.saved_model.save(model, ecosnap_save_path)
+model.save('ml/models/super_model.h5')
+
+# import tensorflowjs as tfjs
+# tfjs.converters.save_keras_model(model, 'ml/models/')
+
+# loaded = tf.saved_model.load("ml/ecosnap/4/saved_model.pb")
+# # predictions
+
+# # Retrieve a batch of images from the test set
+# image_batch, label_batch = validation_dataset.as_numpy_iterator().next()
+# predictions = loaded.predict_on_batch(image_batch).flatten()
+# # Apply a sigmoid since our model returns logits
+# predictions = tf.nn.softmax(predictions)
+
+
+def prep_image(image_url):
+    img = tf.keras.utils.load_img(
+        image_url,
+        target_size=(200, 200)
+    )
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+    return img_array
+
+
+img_array_1 = prep_image('ml/test/plastic-recycling-code-pete-recycle-symbol-isolated-icon-pete-polyethylene-recycling-code-plastic-recycle-symbol-triangle-159234010.jpg')
+
+img_array_5 = prep_image('ml/test/pp-plastic_5.jpeg')
+
+predictions = model.predict(img_array_5)
+predictions
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(predictions)], 100 * np.max(predictions))
+)
