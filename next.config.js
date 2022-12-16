b1b71e679e30f67114e01fb94fcffbd5c8883d76
Alyssa X <hi@alyssax.com>
2022-12-16 16:51:04 +0000

    Check deployed new package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+	webpack: {
+    configure: {
+      experiments: {
+        topLevelAwait: true,
+      },
+    },
+  },
 }
 
 module.exports = nextConfig
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,22 @@
+import loadTf from 'tfjs-node-lambda';
+import { Readable } from 'stream';
+import axios from 'axios';
+
+
 // https://github.com/tensorflow/tfjs-examples/blob/master/firebase-object-detection-node/functions/index.js
 export default async function handler(req, res) {
-	const loadTf = require('tensorflow-lambda')
+	//const loadTf = require('tensorflow-lambda')
 	//const tf = require('@tensorflow/tfjs-node')
-	const tf = await loadTf()
+	//const tf = await loadTf()
+
+	const response = await axios.get(
+		'https://github.com/jlarmstrongiv/tfjs-node-lambda/releases/download/v2.0.10/nodejs12.x-tf2.8.6.br',
+		{ responseType: 'arraybuffer' },
+	);
+	
+	const readStream = Readable.from(response.data);
+	//const tf: typeof import('@tensorflow/tfjs') = await loadTf(readStream);
+	const tf = await loadTf(readStream);
 
 	let Model;
 
