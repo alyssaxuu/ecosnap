17d8a3b94e5874393fd3c8249f49d451894a7067
Alyssa X <hi@alyssax.com>
2022-12-20 02:27:21 +0000

    Remove Firebase

--- pages/components/Firebase.js
+++ pages/components/Firebase.js
@@ -1,5 +1,5 @@
-import db from "../../firebase/firebaseConfig";
-import { doc, getDoc, updateDoc, onSnapshot } from "firebase/firestore";
+//import db from "../../firebase/firebaseConfig";
+//import { doc, getDoc, updateDoc, onSnapshot } from "firebase/firestore";
 
 const saveImage = (image, pred, number) => {
 		return console.log(pred+"oi oi"+number+"ha ha ha"+image);
