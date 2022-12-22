cec46de3cd6f389bec47349ed029b027644016d1
Alyssa X <hi@alyssax.com>
2022-12-22 15:28:32 +0000

    Delete node_modules directory

--- node_modules/react-camera-pro/dist/components/Camera/Camera.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import { CameraProps } from './types';
-export declare const Camera: React.ForwardRefExoticComponent<CameraProps & React.RefAttributes<unknown>>;
--- node_modules/react-camera-pro/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Camera } from './components/Camera/Camera';
-import { CameraType, CameraProps } from './components/Camera/types';
-export { Camera, CameraType, CameraProps };
