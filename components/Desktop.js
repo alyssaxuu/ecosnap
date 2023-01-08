6def3898ad6281e1d17ed1f915bddcb14b5aa33b
Alyssa X <hi@alyssax.com>
2023-01-08 22:00:05 +0000

    New landing page

--- components/Desktop.js
+++ components/Desktop.js
@@ -3,16 +3,61 @@ import styles from "./../styles/Desktop.module.css";
 import Splash from "./Splash";
 
 const Desktop = (props) => {
-	return (
-		<div className={styles.desktop}>
-			{props.check &&
-        	<Splash region={props.region} pred={props.pred} setPred={props.setPred} tensor={props.tensor} setTensor={props.setTensor} setNum={props.setNum} num={props.num} onboarding={props.onboarding} setRegion={props.setRegion} />
-				}
-				<div className={styles.credit}>Made by <a href="https://www.linkedin.com/in/leonorfurtado" target="_blank">Leo</a> & <a href="https://twitter.com/alyssaxuu/" target="_blank">Alyssa X</a></div>
-				<a href="https://twitter.com/intent/tweet?text=Check%20out%20EcoSnap%20to%20learn%20how%20to%20recycle%20your%20plastic%20better%20with%20AI&url=http%3A%2F%2Fecosnap.vercel.app" className={styles.share} target="_blank"><img src="twitter.svg"/> Share</a>
-				<a href="https://github.com/alyssaxuu/ecosnap" target="_blank" className={styles.github}><img src="github.svg"/> View on GitHub</a>
-		</div>
-	)
-}
+  return (
+    <div className={styles.desktop}>
+      {props.check && (
+        <Splash
+          region={props.region}
+          pred={props.pred}
+          setPred={props.setPred}
+          tensor={props.tensor}
+          setTensor={props.setTensor}
+          setNum={props.setNum}
+          num={props.num}
+          onboarding={props.onboarding}
+          setRegion={props.setRegion}
+        />
+      )}
+      <div className={styles.awards}>
+        <img className={styles.award1} src="./award1.svg" />
+        <img className={styles.award2} src="./award2.svg" />
+      </div>
+      <div className={styles.badge}>
+        <img src="logo-with-back.svg" />
+        <div className={styles.badgewrap}>
+          <div className={styles.name}>EcoSnap</div>
+          <div className={styles.credit}>
+            Made by{" "}
+            <a href="https://www.linkedin.com/in/leonorfurtado" target="_blank">
+              Leo
+            </a>{" "}
+            &{" "}
+            <a href="https://twitter.com/alyssaxuu/" target="_blank">
+              Alyssa X
+            </a>
+          </div>
+        </div>
+      </div>
+      <a
+        href="https://twitter.com/intent/tweet?text=Check%20out%20EcoSnap%20to%20learn%20how%20to%20recycle%20your%20plastic%20better%20with%20AI&url=http%3A%2F%2Fecosnap.vercel.app"
+        className={styles.share}
+        target="_blank"
+      >
+        <img src="twitter.svg" /> Share
+      </a>
+      <a
+        href="https://github.com/alyssaxuu/ecosnap"
+        target="_blank"
+        className={styles.github}
+      >
+        <img src="github.svg" /> View on GitHub
+      </a>
+      <div className={styles.qrcode}>
+        <img src="qrcode.png" />
+        <span>Open on your phone ✨</span>
+      </div>
+    </div>
+  );
+};
 
 export default Desktop;
--- components/Onboarding.js
+++ components/Onboarding.js
@@ -1,95 +1,108 @@
-import { progressBarHelper } from "@tensorflow/tfjs-node/dist/callbacks";
 import React, { useState } from "react";
-import styles from "./../styles/Onboarding.module.css"
+import styles from "./../styles/Onboarding.module.css";
 
 const Onboarding = (props) => {
-	const [step, setStep] = useState(1);
+  const [step, setStep] = useState(1);
 
-	const handleStep = () => {
-		if (step < 3) {
-			setStep(step + 1);
-		}
-	}
+  const handleStep = () => {
+    if (step < 3) {
+      setStep(step + 1);
+    }
+  };
 
-	return (
-		<div className={styles.onboarding}>
-			<div className={styles.skip} onClick={() =>{
-				props.setDone(true);
-				props.setOn(false)
-			}}>
-				Skip
-			</div>
-			{ step === 1 &&
-				<div className={styles.image}>
-					<img src="1handright.svg" className={styles.handright}/>
-					<img src="1handleft.svg" className={styles.handleft}/>
-				</div>
-			}
-			{ step === 2 &&
-				<div className={styles.image}>
-					<img src="2robot.svg" className={styles.robot}/>
-					<img src="2smoke.svg" className={styles.smoke}/>
-					<img src="2question.svg" className={styles.question}/>
-				</div>
-			}
-			{ step === 3 &&
-				<div className={styles.image}>
-					<img src="3trash.svg" className={styles.trash}/>
-					<img src="3trees.svg" className={styles.trees}/>
-				</div>
-			}
-			<div className={styles.bottom}>
-			<div className={styles.progress}>
-				<div onClick={() => setStep(1)} className={step === 1 ? styles.active : styles.inactive}></div>
-				<div onClick={() => setStep(2)} className={step === 2 ? styles.active : styles.inactive}></div>
-				<div onClick={() => setStep(3)} className={step === 3 ? styles.active : styles.inactive}></div>
-			</div>
-			{ step === 1 &&
-			<div>
-				<div className={styles.text}>
-					Snap a picture of a plastic code
-				</div>
-				<div className={styles.subtitle}>
-				Look for a tiny triangle with a number inside it from 1 to 7. Keep it in the center of the camera for best results.
-				</div>
-			</div>
-			}
-			{ step === 2 &&
-			<div>
-				<div className={styles.text}>
-					Did the AI get it right?
-				</div>
-				<div className={styles.subtitle}>
-				After taking a picture, we will ask you if the AI recognised your code. If it’s not right, you can teach the AI.
-				</div>
-			</div>
-			}
-			{ step === 3 &&
-			<div>
-				<div className={styles.text}>
-					Learn how to recycle your plastic item
-				</div>
-				<div className={styles.subtitle}>
-				You’ll learn if the item is recyclable or not, and how you should dispose of it.
-				</div>
-			</div>
-			}
-			{step < 3 &&
-			<div className={styles.next} onClick={() => handleStep()}>
-				<img src="arrow.svg"/>
-			</div>
-			}
-			{step === 3 &&
-			<div className={styles.button} onClick={() =>{
-				props.setDone(true);
-				props.setOn(false)
-			}}>
-				Start recycling
-			</div>
-			}
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className={styles.onboarding}>
+      <div
+        className={styles.skip}
+        onClick={() => {
+          props.setDone(true);
+          props.setOn(false);
+        }}
+      >
+        Skip
+      </div>
+      {step === 1 && (
+        <div className={styles.image}>
+          <img src="1handright.svg" className={styles.handright} />
+          <img src="1handleft.svg" className={styles.handleft} />
+        </div>
+      )}
+      {step === 2 && (
+        <div className={styles.image}>
+          <img src="2robot.svg" className={styles.robot} />
+          <img src="2smoke.svg" className={styles.smoke} />
+          <img src="2question.svg" className={styles.question} />
+        </div>
+      )}
+      {step === 3 && (
+        <div className={styles.image}>
+          <img src="3trash.svg" className={styles.trash} />
+          <img src="3trees.svg" className={styles.trees} />
+        </div>
+      )}
+      <div className={styles.bottom}>
+        <div className={styles.progress}>
+          <div
+            onClick={() => setStep(1)}
+            className={step === 1 ? styles.active : styles.inactive}
+          ></div>
+          <div
+            onClick={() => setStep(2)}
+            className={step === 2 ? styles.active : styles.inactive}
+          ></div>
+          <div
+            onClick={() => setStep(3)}
+            className={step === 3 ? styles.active : styles.inactive}
+          ></div>
+        </div>
+        {step === 1 && (
+          <div>
+            <div className={styles.text}>Snap a picture of a plastic code</div>
+            <div className={styles.subtitle}>
+              Look for a tiny triangle with a number inside it from 1 to 7. Keep
+              it in the center of the camera for best results.
+            </div>
+          </div>
+        )}
+        {step === 2 && (
+          <div>
+            <div className={styles.text}>Did the AI get it right?</div>
+            <div className={styles.subtitle}>
+              After taking a picture, we will ask you if the AI recognised your
+              code. If it’s not right, you can teach the AI.
+            </div>
+          </div>
+        )}
+        {step === 3 && (
+          <div>
+            <div className={styles.text}>
+              Learn how to recycle your plastic item
+            </div>
+            <div className={styles.subtitle}>
+              You’ll learn if the item is recyclable or not, and how you should
+              dispose of it.
+            </div>
+          </div>
+        )}
+        {step < 3 && (
+          <div className={styles.next} onClick={() => handleStep()}>
+            <img src="arrow.svg" />
+          </div>
+        )}
+        {step === 3 && (
+          <div
+            className={styles.button}
+            onClick={() => {
+              props.setDone(true);
+              props.setOn(false);
+            }}
+          >
+            Start recycling
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Onboarding;
